c13b0e4ea6da0b1af3018c25c9e4ef1944790051
Pavel Kogut <kogut.kogut1@mail.ru>
2021-06-19 12:34:45 +0500

    Посадка верстки на реакт

--- script.js
+++
@@ -1,9 +0,0 @@
-function Welcome(props) {
-  return <h1>Привет, {props.name}</h1>;
-}
-
-const element = <Welcome name="Алиса" />;
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
